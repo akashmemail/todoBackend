01a77daa0dcd6e4d2591bbc8bf1d12b2c8b28d6b
akashmemail <akashmemail@gmail.com>
2026-09-18 08:21:29 +0530

    chand routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,22 +2,22 @@ const express = require("express");
 
 const rout = express.Router();
 
-const regform = require("../../backend/controllers/auth/registion.controller.js");
+const regform = require("../controllers/auth/registion.controller.js");
 
-const loginform = require("../../backend/controllers/auth/login.controller.js");
+const loginform = require("../controllers/auth/login.controller.js");
 
-const authveryfay = require("../../backend/middleware/auth.middleware.js");
+const authveryfay = require("../middleware/auth.middleware.js");
 
-const profileform = require("../../backend/controllers/auth/profile.controller.js");
+const profileform = require("../controllers/auth/profile.controller.js");
 
-const maketodo = require("../../backend/controllers/todo/todo.controller.js");
+const maketodo = require("../controllers/todo/todo.controller.js");
 
-const todoedite = require("../../backend/controllers/todo/todoedite.controller.js");
-const profile = require("../../backend/controllers/auth/profile.controller.js");
+const todoedite = require("../controllers/todo/todoedite.controller.js");
+// const profile = require("../../backend/controllers/auth/profile.controller.js");
 
-const gettodo = require("../../backend/controllers/todo/todoget.controller.js");
+const gettodo = require("../controllers/todo/todoget.controller.js");
 
-const delet = require("../../backend/controllers/todo/deletetodo.controller.js");
+const delet = require("../controllers/todo/deletetodo.controller.js");
 
 rout.post("/todos", authveryfay, maketodo);
 
